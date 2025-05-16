1f63eeabcb16d9b1ea4318a7fe555e04b961acc6
kiii <muhammadrifkyalfajri@gmail.com>
2025-05-16 18:40:05 +0700

    Update tubes.go

--- tubes.go
+++ tubes.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 /* kita ingin membuat sebuah applikasi untuk simulasi pasar saham, dengan tampilan awal dengan beberapa pilihan.
 Memperlihatkan daftar saham, beli seham, jual saham, menampilkan portofolio, menampilkan statistik untung/rugi
@@ -8,7 +11,8 @@ kita beranggapan bahwa tiap memjalankan applikasi ini adalah orang yang berbeda
 */
 
 type pengguna struct {
-	nama, SahamDimiliki string
+	nama, SahamDimiliki            string
+	nilaiSahamDimiliki, untungRugi float64
 }
 
 type transaksi struct {
@@ -17,8 +21,8 @@ type transaksi struct {
 }
 
 type saham struct {
-	id, namasaham      string
-	volume, hargaSaham float64
+	id, namasaham                  string
+	volume, hargaSaham, nilaiSaham float64
 }
 
 type tabtr [150]transaksi //array transaksi
@@ -27,18 +31,17 @@ type tabsa [10]saham      // array saham
 
 var daftarSaham tabsa
 
-var transaksi tabtr
+var Transaksi tabtr
 var jumlahTransaksi int = 0
 
 var portofolio tabpe
 var jumlahPortofolio int = 0
 
-var saldo int = 1000000
-var modalAwal int = 1000000
+var saldo float64 = 10000000 //karena trading saham tanpa resiko nyata, asumsikan kita menyedikan uang virtual sebagai bahan pembelajaran
 
 func inisialisasiDataSaham() {
 	var dataSaham tabsa
-	dataSaham[0] = saham{id: "BBCA", namasaham: "Bank Central Asia", hargaSaham: 9275} //Harga Saham dalam 1 lebar saham
+	dataSaham[0] = saham{id: "BBCA", namasaham: "Bank Central Asia", hargaSaham: 9275, volume: 10000, nilaiSaham: 6788889} //Harga Saham dalam 1 lebar saham
 	dataSaham[1] = saham{id: "BBRI", namasaham: "Bank rakyat Indonesia", hargaSaham: 4260}
 	dataSaham[2] = saham{id: "BMRI", namasaham: "Bank Mandiri", hargaSaham: 5275}
 	dataSaham[3] = saham{id: "TLKM", namasaham: "Telkom Indonesia", hargaSaham: 2660}
@@ -59,13 +62,13 @@ func tampilkanDaftarSaham() { // Daftar-daftar saham yang dapat dibeli
 	}
 }
 
-func beli() { //beli saham apa, kode sham
+func beli() { //beli: nama saham, kode saham, beli berdasarkan lembar saham(bukan nilai dlm rupiah) dan pastikan saldo masih cukup,
 
 }
 func jual() {
 
 }
-func portofolioPengguna() { //nama pengguna, saham dimiliki, nilai saham, keuntungan, kerugian
+func portofolioPengguna(nama, sahamDimiliki, nilaiSHDimiliki, untungRugi tabpe) { //input nama pengguna lalu buat var saham dimiliki, nilai saham, keuntungan, kerugian
 
 }
 func tampilkanPortofolioPengguna() {
@@ -74,12 +77,39 @@ func tampilkanPortofolioPengguna() {
 func tampilkanStatistikUntungRugi() {
 
 }
-func simulasiTrading() { //masih dalam perencanaan tergantung boleh atau tidak penggunaan import "math/rand"
+func hitungUntungRugi() {
+	//kurang lebih saldo yang "sudah dibelikan saham" akan dihitung,
+	//pertambahan nilai atau pengurangan nilai dari saham setelah melakukan simulasi trading
+
+}
+func simulasiTrading() { //n untuk berapa lama menahan
 	/* idenya adalah asumsikan bahwa naik turun harga saham memiliki kemungkinan untung 3 : 1 rugi
 	anggap kita mempunya angka random 1-5 dengan jika dapat 1 tidak ada perubahan jika dapat 2 Harga turun sekian dalam %
 	jika dapat 3-5 harga naik sekian dalam %. anggap tiap kenaikkan adalah dalam rentang 1 bulan sehingga masukkan
 	misal 4 jadi kita seperti perkembangan harga saham dalam 4 bulan atau 4 kali naik / turun */
-
+	var n int
+	fmt.Println("Simulasi pergerakan harga saham selama 1 bulan per langkah, ingin menahan berapa lama: ")
+	fmt.Scan(&n)
+	for i := 1; i <= n; i++ {
+		// random 1–5
+		switch rand.Intn(5) + 1 { // + 1 karena dari nol
+		case 1:
+			// tidak ada perubahan
+		case 2:
+			daftarSaham[i].hargaSaham *= 0.98 // turun 2%
+			daftarSaham[i].nilaiSaham *= 0.98
+		case 3:
+			daftarSaham[i].hargaSaham *= 1.03 // naik 3%
+			daftarSaham[i].nilaiSaham *= 1.03
+		case 4:
+			daftarSaham[i].hargaSaham *= 1.04 // naik 4%
+			daftarSaham[i].nilaiSaham *= 1.04
+		case 5:
+			daftarSaham[i].hargaSaham *= 1.05 // naik 5%
+			daftarSaham[i].nilaiSaham *= 1.05
+		}
+	}
+	fmt.Println("Harga telah diperbarui.")
 }
 
 func urutDataDescedingHarga(data *tabsa, ndata int) { //untuk mengurutkan data saham dari besar ke kecil berdasarkan Harga saham
@@ -122,8 +152,32 @@ func urutDataDescedingVolume(data *tabsa, ndata int) { //untuk mengurutkan data 
 	}
 }
 
+func pilihan(pilih *int) { //pengguna memilih akan melakukan apa
+	for {
+		fmt.Printf("Pilih hal apa yang akan anda lakukan: \n1. Tampilkan daftar saham. \n2. Tampilkan portofolio anda. \n3.Beli saham. \n4.Jual saham. \n5. Tampilkan statistik keuntungan/kerugian. \n6. Melakukan simulasi saham. \n7. Keluar.")
+		fmt.Scan(pilih)
+		switch *pilih {
+		case 1: //memanggil fungsi daftar saham
+			tampilkanDaftarSaham()
+		case 2: //fungsi porto
+
+		case 3: //
+
+		case 4:
+
+		case 5:
+
+		case 6:
+			simulasiTrading()
+		default:
+			return
+		}
+	}
+}
+
 func main() {
 	inisialisasiDataSaham()
-	var hargaSaham float64
+	var pilih int
+	pilihan(pilih)
 
 }
